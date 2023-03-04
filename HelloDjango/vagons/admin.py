86d316d30acfd253f296b0faf6093ba55a19591f
vladiuse <vladiuse@gmail.com>
2023-03-04 20:52:41 +0300

    add date hierarchy

--- HelloDjango/vagons/admin.py
+++ HelloDjango/vagons/admin.py
@@ -6,5 +6,6 @@ from .models import ClientDoc
 class ClientDocAdmin(admin.ModelAdmin):
     list_display = ['name', 'load_date', 'document_date', 'description', 'document_file']
     exclude = ['document']
+    date_hierarchy = 'document_date'
 
 admin.site.register(ClientDoc, ClientDocAdmin)
